9b420be3f1f6950bc08cd5008267382caa6ccb3d
alex <alex@agilegeeks.ro>
2019-05-16 21:00:15 +0300

    Created a basic skeleton for the general layout

--- src/App.js
+++ src/App.js
@@ -1,50 +1,74 @@
 import React, { Component } from 'react';
-import { Button, Card, Divider, Icon, Breadcrumb, Breadcrumbs, H3, Navbar, Alignment, Elevation } from '@blueprintjs/core';
+import { Icon, Button } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 
-const BREADCRUMBS = [
-    { href: '/users', icon: 'folder-close', text: 'Users' },
-    { href: '/users/janet', icon: 'folder-close', text: 'Janet' },
-    { icon: 'document', text: 'image.jpg' },
-];
 
-class App extends Component {
-
-    renderCurrentBreadcrumb = ({ text, ...restProps }) => {
-        // customize rendering of last breadcrumb
-        return <Breadcrumb {...restProps}>{text} <Icon icon="star" /></Breadcrumb>;
+export default class App extends Component {
+    state = {
+        open: true
     };
 
+    showSidenav = (e) => {
+        this.setState({open: true});
+    }
+
+    hideSidenav = (e) => {
+        this.setState({open: false});
+    }
+
     render() {
+        const { open } = this.state;
         return (
-            <div className="wrapper">
-                <div className="sidebar">
-                    <Card elevation={Elevation.TWO}>
-                        <H3>Test Title</H3>
-                        <Divider/>
-                        <ul>
-                            <li>item 1</li>
-                            <li>item 1</li>
-                            <li>item 1</li>
-                        </ul>
-                    </Card>
+            <div className="ag-grid-container">
+                <div className="ag-menu-icon">
+                    <Button minimal small onClick={this.showSidenav} icon={IconNames.LIST}>
+                        {/* <Icon icon={IconNames.LIST}/> */}
+                    </Button>
                 </div>
-                <div className="main-content">
-                    <Navbar>
-                        <Navbar.Group align={Alignment.LEFT}>
-                            <Navbar.Heading>Blueprint</Navbar.Heading>
-                            <Navbar.Divider />
-                            <Button className="bp3-minimal" icon="home" text="Home" />
-                            <Button className="bp3-minimal" icon="document" text="Files" />
-                        </Navbar.Group>
-                    </Navbar>
-                    <Breadcrumbs
-                        currentBreadcrumbRenderer={this.renderCurrentBreadcrumb}
-                        items={BREADCRUMBS}
-                    />
+                <div className="ag-header"></div>
+                <div className={open ? 'ag-sidenav active' : 'ag-sidenav'}>
+                    <div className="ag-sidenav__close-icon">
+                        <Icon icon={IconNames.CHEVRON_LEFT} onClick={this.hideSidenav}/>
+                    </div>
+                    <ul className="ag-sidenav__list">
+                        <li className="ag-sidenav__list-item">Item One</li>
+                        <li className="ag-sidenav__list-item">Item Two</li>
+                        <li className="ag-sidenav__list-item">Item Three</li>
+                        <li className="ag-sidenav__list-item">Item Four</li>
+                        <li className="ag-sidenav__list-item">Item Five</li>
+                    </ul>
                 </div>
+                <div className="ag-main">
+                    <div className="ag-main-header">
+                        <div className="main-header__heading">Hello User</div>
+                        <div className="main-header__updates">Recent Items</div>
+                    </div>
+                    <div className="ag-main-overview">
+                        <div className="ag-overviewcard">
+                            <div className="ag-overviewcard__icon">Overview</div>
+                            <div className="ag-overviewcard__info">Card</div>
+                        </div>
+                        <div className="ag-overviewcard">
+                            <div className="ag-overviewcard__icon">Overview</div>
+                            <div className="ag-overviewcard__info">Card</div>
+                        </div>
+                        <div className="ag-overviewcard">
+                            <div className="ag-overviewcard__icon">Overview</div>
+                            <div className="ag-overviewcard__info">Card</div>
+                        </div>
+                        <div className="ag-overviewcard">
+                            <div className="ag-overviewcard__icon">Overview</div>
+                            <div className="ag-overviewcard__info">Card</div>
+                        </div>
+                    </div>
+                    <div className="ag-main-cards">
+                        <div className="ag-card">Card</div>
+                        <div className="ag-card">Card</div>
+                        <div className="ag-card">Card</div>
+                    </div>
+                </div>
+                <div className="ag-footer"></div>
             </div>
         );
     }
 }
-
-export default App;
